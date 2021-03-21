438010180fa9ac73ffcfa323c87b41de425ec4cd
Van Schroeder <vanschroeder@mesmer.local>
2021-03-21 15:02:47 -0500

    updated dependencies / refactored webpack config / updated package / rebuilt source

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,42 @@
-/**
- * Created by van on 7/8/17.
- */
-var path = require('path');
-module.exports ={
+const path = require("path");
+module.exports = [{
+    externals: [],
     entry: {
-        entry: './src/index.js'
+        entry: "./src/index.js"
     },
     output: {
-        filename: 'ofs-utils.js',
-        path: path.resolve(__dirname, 'dist'),
+        filename: "ofs-utils.js",
+        path: path.resolve(__dirname, "dist"),
         library: "ofsUtils",
         libraryTarget: "umd",
     }
-};
+}, {
+    externals: [],
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "ofs-utils.window.js",
+        libraryTarget: "window",
+        library: "ofs-utils",
+    },
+    module: {},
+}, {
+    externals: [
+        // webpackRxjsExternals(),
+    ],
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "ofs-utils.node.js",
+        libraryTarget: "commonjs",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+        ],
+    }
+}];
